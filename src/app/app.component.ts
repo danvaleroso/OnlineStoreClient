966bd2a066ac70984cdb224d1cc4a3995520c84e
Dan <69251191+danvaleroso@users.noreply.github.com>
2024-05-04 23:39:37 +0800

    added api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'web';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/products').subscribe({
+      next: response => console.log(response),
+      error: err => console.log(err),
+      complete: () => {
+        console.log("request completed");
+      }
+    });
+  }
 }
