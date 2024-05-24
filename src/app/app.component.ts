3cc5df24ac8161294991ad0416a5a25928989ffb
Dan <69251191+danvaleroso@users.noreply.github.com>
2024-05-24 20:30:09 +0800

    Added shared module and shop module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { Product } from './models/product';
-import { Pagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
@@ -11,17 +8,10 @@ import { Pagination } from './models/pagination';
 
 export class AppComponent implements OnInit {
   title = 'web';
-  products: Product[] = [];
 
-  constructor(private http: HttpClient) {}
+  constructor() {}
 
   ngOnInit(): void {
-    this.http.get<Pagination<Product[]>>('https://localhost:5001/api/products?pageSize=50').subscribe({
-      next: response => this.products = response.data,
-      error: err => console.log(err),
-      complete: () => {
-        console.log("request completed");
-      }
-    });
+    
   }
 }
