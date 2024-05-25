c42efad5bb9241a727308e4e8e8c933b71e88b7e
Dan <69251191+danvaleroso@users.noreply.github.com>
2024-05-25 16:34:13 +0800

    Added pagination

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -3,6 +3,7 @@ import { Product } from '../shared/models/product';
 import { ShopService } from './shop.service';
 import { Brand } from '../shared/models/type';
 import { Type } from '../shared/models/brand';
+import { ShopParams } from '../shared/models/shopParams';
 
 @Component({
   selector: 'app-shop',
@@ -13,16 +14,15 @@ export class ShopComponent implements OnInit {
   products: Product[] = [];
   brands: Brand[] = [];
   types: Type[] = [];
+  shopParams = new ShopParams();
 
-  brandIdSelected = 0;
-  typeIdSelected = 0;
-
-  sortSelected = "name";
   sortOptions = [
     {name: 'Alphabetical', value: 'name'},
     {name: 'Price: Low to High', value: 'priceAsc'},
     {name: 'Price: High to Low', value: 'priceDesc'},
-  ]
+  ];
+
+  totalCount = 0;
 
   constructor(private shopService: ShopService) {
     
@@ -35,8 +35,13 @@ export class ShopComponent implements OnInit {
   }
 
   getProducts() {
-    this.shopService.getProducts(this.brandIdSelected, this.typeIdSelected, this.sortSelected).subscribe({
-      next: response => this.products = response.data,
+    this.shopService.getProducts(this.shopParams).subscribe({
+      next: response => {
+        this.products = response.data;
+        this.shopParams.pageNumber = response.pageIndex;
+        this.shopParams.pageSize = response.pageSize;
+        this.totalCount = response.count;
+      },
       error: error => console.log(error)
     })
   }
@@ -56,17 +61,24 @@ export class ShopComponent implements OnInit {
   }
 
   onBrandSelected(brandId: number) {
-    this.brandIdSelected = brandId;
+    this.shopParams.brandId = brandId;
     this.getProducts();
   }
 
   onTypeSelected(typeId: number) {
-    this.typeIdSelected = typeId;
+    this.shopParams.typeId = typeId;
     this.getProducts();
   }
 
   onSortSelected(event: any) {
-    this.sortSelected = event.target.value;
+    this.shopParams.sort = event.target.value;
     this.getProducts();
+  }
+
+  onPageChanged(event: any) {
+    if (this.shopParams.pageNumber !== event.page) {
+      this.shopParams.pageNumber = event.page;
+      this.getProducts();
+    }
   }
 }
--- src/app/shop/shop.service.ts
+++ src/app/shop/shop.service.ts
@@ -4,6 +4,7 @@ import { Product } from '../shared/models/product';
 import { Pagination } from '../shared/models/pagination';
 import { Brand } from '../shared/models/type';
 import { Type } from '../shared/models/brand';
+import { ShopParams } from '../shared/models/shopParams';
 
 @Injectable({
   providedIn: 'root'
@@ -16,18 +17,22 @@ export class ShopService {
 
   }
 
-  getProducts(brandId?: number, typeId?: number , sort? :string) {
+  getProducts(shopParams: ShopParams) {
     let params = new HttpParams();
 
-    if(brandId) params = params.append('brandId', brandId);
-    if(typeId) params = params.append('typeId', typeId);
-    if(sort) params = params.append('sort', sort);
+    if(shopParams.brandId > 0) params = params.append('brandId', shopParams.brandId);
+    if(shopParams.typeId) params = params.append('typeId', shopParams.typeId);
+    params = params.append('sort', shopParams.sort);
+    params = params.append('pageNumber', shopParams.pageNumber);
+    params = params.append('pageSize', shopParams.pageSize);
     
     return this.http.get<Pagination<Product[]>>(this.baseUrl + "products", {params: params});
   }
+
   getBrands() {
     return this.http.get<Brand[]>(this.baseUrl + "products/brands");
   }
+
   getTypes() {
     return this.http.get<Type[]>(this.baseUrl + "products/types");
   }
